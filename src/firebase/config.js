5dd7d635e5338b6e7da21044b563d779e2940e84
Sayar2003 <145477124+Sayar2003@users.noreply.github.com>
2026-06-01 20:21:47 +0530

    Fixing firebase config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -8,6 +8,8 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
+console.log('Firebase config:', firebaseConfig);
+
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
